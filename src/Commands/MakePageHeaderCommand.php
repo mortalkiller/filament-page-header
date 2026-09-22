9c9ede5883f0e171ed0374fb6547d53cd1dc6fc8
Pedro Monteiro <pedro.monteiro.ps@gmail.com>
2026-09-22 17:44:01 +0100

    test: preserve normalized panel resolution

--- src/Commands/MakePageHeaderCommand.php
+++ src/Commands/MakePageHeaderCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MortalKiller\FilamentPageHeader\Commands;
 
 use Filament\Facades\Filament;
+use Filament\FilamentManager;
 use Filament\Panel;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Resources\Pages\EditRecord;
@@ -86,7 +87,7 @@ final class MakePageHeaderCommand extends Command
 
         $panelOption = $this->option('panel');
 
-        if (filled($panelOption) && ! array_key_exists((string) $panelOption, $panels)) {
+        if (filled($panelOption) && app(FilamentManager::class)->getPanel((string) $panelOption, isStrict: false) === null) {
             throw new RuntimeException("Filament panel [{$panelOption}] was not found.");
         }
 
